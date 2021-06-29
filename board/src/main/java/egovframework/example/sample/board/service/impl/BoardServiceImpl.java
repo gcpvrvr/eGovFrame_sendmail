21016683c4b7a7437a4a608101b9bc7b4c123481
gcpvrvr <gcpvrvr@gmail.com>
2021-06-29 19:56:24 +0900

    210629:1956

--- board/src/main/java/egovframework/example/sample/board/service/impl/BoardServiceImpl.java
+++ board/src/main/java/egovframework/example/sample/board/service/impl/BoardServiceImpl.java
@@ -32,120 +32,123 @@ import egovframework.rte.fdl.idgnr.EgovIdGnrService;
  * @Class Name : EgovSampleServiceImpl.java
  * @Description : Sample Business Implement Class
  * @Modification Information
- * @ @ 수정일 수정자 수정내용 @ --------- --------- ------------------------------- @
- *   2009.03.16 최초생성
+ * @
+ * @  수정일      수정자              수정내용
+ * @ ---------   ---------   -------------------------------
+ * @ 2009.03.16           최초생성
  *
  * @author 개발프레임웍크 실행환경 개발팀
  * @since 2009. 03.16
  * @version 1.0
  * @see
  *
- * 		Copyright (C) by MOPAS All right reserved.
+ *  Copyright (C) by MOPAS All right reserved.
  */
 
 @Service("boardService")
 public class BoardServiceImpl extends EgovAbstractServiceImpl implements BoardService {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(BoardServiceImpl.class);
+   private static final Logger LOGGER = LoggerFactory.getLogger(BoardServiceImpl.class);
 
-	/** SampleDAO */
-	// TODO ibatis 사용
-//	@Resource(name = "sampleDAO")
-//	private SampleDAO sampleDAO;
-	// TODO mybatis 사용
-	@Resource(name = "boardMapper")
-	private BoardMapper boardDAO;
+   /** SampleDAO */
+   // TODO ibatis 사용
+//   @Resource(name = "sampleDAO")
+//   private SampleDAO sampleDAO;
+   // TODO mybatis 사용
+     @Resource(name="boardMapper")
+      private BoardMapper boardDAO;
 
-	/** ID Generation */
-	@Resource(name = "egovIdGnrService")
-	private EgovIdGnrService egovIdGnrService;
+   /** ID Generation */
+   @Resource(name = "egovIdGnrService")
+   private EgovIdGnrService egovIdGnrService;
 
-	/**
-	 * 글을 등록한다.
-	 * 
-	 * @param vo - 등록할 정보가 담긴 SampleVO
-	 * @return 등록 결과
-	 * @exception Exception
-	 */
+   /**
+    * 글을 등록한다.
+    * @param vo - 등록할 정보가 담긴 SampleVO
+    * @return 등록 결과
+    * @exception Exception
+    */
 
-	@Override
-	public String insertBoard(BoardVO vo) throws Exception {
-		LOGGER.debug(vo.toString());
+   
+   public String insertBoard(BoardVO vo) throws Exception {
+      LOGGER.debug(vo.toString());
 
-//		/** ID Generation Service */
-//		String id = egovIdGnrService.getNextStringId();
-//		vo.setId(id);
-//		LOGGER.debug(vo.toString());
+//      /** ID Generation Service */
+//      String id = egovIdGnrService.getNextStringId();
+//      vo.setId(id);
+//      LOGGER.debug(vo.toString());
 
-		boardDAO.insertBoard(vo);
-		return vo.getIdx();
-	}
+      boardDAO.insertBoard(vo);
+      return vo.getIdx();
+   }
 
-	/**
-	 * 글을 수정한다.
-	 * 
-	 * @param vo - 수정할 정보가 담긴 SampleVO
-	 * @return void형
-	 * @exception Exception
-	 */
-	@Override
-	public void updateBoard(BoardVO vo) throws Exception {
-		boardDAO.updateBoard(vo);
-	}
+   /**
+    * 글을 수정한다.
+    * @param vo - 수정할 정보가 담긴 SampleVO
+    * @return void형
+    * @exception Exception
+    */
+   
+   public void updateBoard(BoardVO vo) throws Exception {
+      boardDAO.updateBoard(vo);
+   }
 
-	/**
-	 * 글을 삭제한다.
-	 * 
-	 * @param vo - 삭제할 정보가 담긴 SampleVO
-	 * @return void형
-	 * @exception Exception
-	 */
+   /**
+    * 글을 삭제한다.
+    * @param vo - 삭제할 정보가 담긴 SampleVO
+    * @return void형
+    * @exception Exception
+    */
 
-	@Override
-	public void deleteBoard(BoardVO vo) throws Exception {
-		boardDAO.deleteBoard(vo);
-	}
+   @Override
+   public void deleteBoard(BoardVO vo) throws Exception {
+      boardDAO.deleteBoard(vo);
+   }
 
-	/**
-	 * 글을 조회한다.
-	 * 
-	 * @param vo - 조회할 정보가 담긴 SampleVO
-	 * @return 조회한 글
-	 * @exception Exception
-	 */
-	@Override
-	public BoardVO selectBoard(BoardVO vo) throws Exception {
-		BoardVO resultVO = boardDAO.selectBoard(vo);
-		if (resultVO == null)
-			throw processException("info.nodata.msg");
-		return resultVO;
-	}
+   /**
+    * 글을 조회한다.
+    * @param vo - 조회할 정보가 담긴 SampleVO
+    * @return 조회한 글
+    * @exception Exception
+    */
+   
+   @Override
+   public BoardVO selectBoard(BoardVO vo) throws Exception {
+      BoardVO resultVO = boardDAO.selectBoard(vo);
+      if (resultVO == null)
+         throw processException("info.nodata.msg");
+      return resultVO;
+   }
 
-	/**
-	 * 글 목록을 조회한다.
-	 * 
-	 * @param searchVO - 조회할 정보가 담긴 VO
-	 * @return 글 목록
-	 * @exception Exception
-	 */
-	@Override
-	public List<?> selectBoardList(BoardVO searchVO) throws Exception {
-		return boardDAO.selectBoardList(searchVO);
-	}
+   /**
+    * 글 목록을 조회한다.
+    * @param searchVO - 조회할 정보가 담긴 VO
+    * @return 글 목록
+    * @exception Exception
+    */
+   
+   @Override
+   public List<?> selectBoardList(BoardVO searchVO) throws Exception {
+      return boardDAO.selectBoardList(searchVO);
+   }
 
-	/**
-	 * 글 총 갯수를 조회한다. @param searchVO - 조회할 정보가 담긴 VO @return 글 총 갯수 @exception
-	 */
-	@Override
-	public int selectBoardListTotCnt(BoardVO searchVO) {
-		return boardDAO.selectBoardListTotCnt(searchVO);
-	}
-	
-	@Override
-	public String selectLoginCheck(BoardVO searchVO) {
-		return boardDAO.selectLoginCheck(searchVO);
-	}
-	
-	
+   /**
+    * 글 총 갯수를 조회한다.
+    * @param searchVO - 조회할 정보가 담긴 VO
+    * @return 글 총 갯수
+    * @exception
+    */
+   
+   @Override
+   public int selectBoardListTotCnt(BoardVO searchVO) {
+      return boardDAO.selectBoardListTotCnt(searchVO);
+   }
+
+   
+   public String selectLoginCheck(BoardVO searchVO) {
+      return boardDAO.selectLoginCheck(searchVO);
+   }
+
+   
 
 }
